59619d7b90d72d08e886947f721c6c5880f7606f
victor <zhangx.geo@yahoo.com>
2018-08-03 21:25:17 -0700

    bug fix for b2.x1 <= b2.x0

--- bronze/2017-12/billboard.cpp
+++ bronze/2017-12/billboard.cpp
@@ -21,20 +21,23 @@ int area(board &b)
 int
 overlap(board &b1, board &b2)
 {
-    if (b2.x0 >= b1.x1 || b2.x1 <= b2.x0 || b2.y0 >= b1.y1 || b2.y1 <= b1.y0) return 0;
+    if (b2.x0 >= b1.x1 || b2.x1 <= b1.x0 || b2.y0 >= b1.y1 || b2.y1 <= b1.y0) return 0;
 
     int x0 = max(b1.x0, b2.x0);
     int y0 = max(b1.y0, b2.y0);
     int x1 = min(b1.x1, b2.x1);
     int y1 = min(b1.y1, b2.y1);
 
-    return area(x0, y0, x1, y1);
+    int a = area(x0, y0, x1, y1);
+    cout << "overlap " << a << endl;
+    return a;
 }
 
 int main()
 {
     board b[3];  // 2 boards, truck
     ifstream in("billboard.in");
+    ofstream out("billboard.out");
 
     for (int i = 0; i< 3; ++i) {
         in >> b[i].x0;
@@ -45,5 +48,6 @@ int main()
 
     int a = area(b[0]) + area(b[1]) - overlap(b[0], b[2]) - overlap(b[1], b[2]);
     cout << a << endl;
+    out << a << endl;
 }
 
